bb0e865e809503293e186997c8978ebe6407f7f9
Kelsey Harris <kelseydelta@gmail.com>
2019-09-13 10:10:40 -0800

    switch

--- lab5.cpp
+++ lab5.cpp
@@ -2,4 +2,23 @@
 /*Kelsey Harris*/
 /*9/13/19*/
 /*input number, and do something with the number*/
+/*use switch-statement*/
 
+#include <isostream>
+
+int main()
+
+{
+	switch (n % 10)
+	{
+	case 1:
+		std::cout << "divedide by 2";
+		break;
+	case 2:
+		std::cout << "divedide by 5";
+		break;
+	case 3:
+		std::cout << "dividede by 10";
+		break;
+	}
+}
